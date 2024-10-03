e83fc9e07665753e2cc57fca00965d6e42b392c3
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2024-10-03 13:18:10 -0300

    Add files via upload

---
+++ processing/similaridade_semantica.py
@@ -0,0 +1,22 @@
+# processing/similaridade_semantica.py
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+
+def calcular_similaridade_semantica(model, sentences_dzubukua, sentences_arcaico, sentences_moderno):
+    """
+    Função para calcular a similaridade semântica entre as frases de três idiomas diferentes.
+    """
+    all_sentences = sentences_dzubukua + sentences_arcaico + sentences_moderno
+    embeddings = model.encode(all_sentences, batch_size=32, normalize_embeddings=True)
+
+    # Separar os embeddings de cada idioma
+    embeddings_dzubukua = embeddings[:len(sentences_dzubukua)]
+    embeddings_arcaico = embeddings[len(sentences_dzubukua):len(sentences_dzubukua) + len(sentences_arcaico)]
+    embeddings_moderno = embeddings[len(sentences_dzubukua) + len(sentences_arcaico):]
+
+    # Calcular similaridade de cosseno
+    similarity_arcaico_dzubukua = cosine_similarity(embeddings_dzubukua, embeddings_arcaico).diagonal()
+    similarity_moderno_dzubukua = cosine_similarity(embeddings_dzubukua, embeddings_moderno).diagonal()
+    similarity_arcaico_moderno = cosine_similarity(embeddings_arcaico, embeddings_moderno).diagonal()
+
+    return similarity_arcaico_dzubukua, similarity_moderno_dzubukua, similarity_arcaico_moderno
